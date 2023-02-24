477ad077690991257c124adfcf58875de7ec0014
action0709 <action0709@mail.ru>
2023-02-24 22:08:56 +0400

    Задачи 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        task1();
+        //task2();
     }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        // Пишем код для задачи 1
+        byte clientOS = 0;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Версия приложения не  определена.");
+        }
+    }
+
 }
+    // public static void task2 () {
+     //   System.out.println("Задача 2");
+  //      ... // Пишем код для задачи 2
